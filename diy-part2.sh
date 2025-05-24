35a898a92b9e19d232da10502dd0c65dcad0e994
z2283975 <z2283975@163.com>
2025-05-24 16:11:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -53,7 +53,4 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-# istore
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
-git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/istore package/istore
+
